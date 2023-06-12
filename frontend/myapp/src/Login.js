f43015c0754e534c74cdde4c0bd2131bc7633bf9
Karan Bhanushali <120624674+kb787@users.noreply.github.com>
2023-06-12 16:46:20 +0530

    Update Login.js

--- frontend/myapp/src/Login.js
+++ frontend/myapp/src/Login.js
@@ -1,6 +1,36 @@
+import axios from 'axios' ;
+import {useNavigate} from 'react-router-dom' ;
+import {message} from 'antd' ;
+import {useState} from 'react' ;
+ 
 const Login = () => 
 {
-  return (  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  var navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      var loginResponse = await axios.post(
+        'http://localhost:3500/api/users/postLogin',
+        {
+          email: email,
+          password: password,
+        }
+      );
+      console.log(loginResponse);
+
+      if (loginResponse.data && loginResponse.data.success) {
+        message.success(' Successfully logged in ');
+        navigate('/Dashboard');
+      }
+    } catch (error) {
+      console.log(error);
+      message.error(' Server side occured ');
+    }
+  };
+  return (
    <div className = "Login">
        <div className = "Register">
     <h2 className = "registerHeading">Verify your credentials</h2>    
@@ -10,6 +40,8 @@ const Login = () =>
       className="registerFormInput"
       placeholder="Enter your email"
       required="true"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
     />
   </div>
   <br />
@@ -19,10 +51,12 @@ const Login = () =>
       className="registerFormInput"
       placeholder="Enter your password"
       required="true"
+      value = {password}
+      onChange={(e) => setPassword(e.target.value)}
     />
   </div>
   <br />
-  <button type="button" className="registerButton">
+  <button type="button" className="registerButton" onClick={handleLogin}>
     Login
   </button>  
     </div> 
